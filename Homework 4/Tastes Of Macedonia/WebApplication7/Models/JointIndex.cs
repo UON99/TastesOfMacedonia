976209f3e31774fe712e0816cb99f512fd6c9b99
UON99 <65608611+UON99@users.noreply.github.com>
2021-01-31 01:55:59 +0100

    Hotfix
    changed the JointIndex class --- removed unnecessary properties from the class

--- Homework 4/Tastes Of Macedonia/WebApplication7/Models/JointIndex.cs
+++ Homework 4/Tastes Of Macedonia/WebApplication7/Models/JointIndex.cs
@@ -7,10 +7,5 @@ namespace WebApplication7.Models
         public List<favorite> favorites { get; set; }
         public List<mytable> mytables { get; set; }
         public List<Rating> ratings { get; set; }
-        public string name { get; set; }
-        public string cuisine { get; set; }
-        public string opening_hours { get; set; }
-        public string website { get; set; }
-        public string phone { get; set; }
     }
 }
